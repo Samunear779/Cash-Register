a7d3f12801876b5b159918f2d8ff1a7fa3d0e633
samunear779 <samunear779@ed.amdsb.ca>
2016-10-16 13:22:10 -0400

    Final Copy of Cash Registor

--- Cash Register/Form1.Designer.cs
+++ Cash Register/Form1.Designer.cs
@@ -40,7 +40,11 @@
             this.print = new System.Windows.Forms.Button();
             this.newOrder = new System.Windows.Forms.Button();
             this.outputLabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.calculateTotals = new System.Windows.Forms.Button();
+            this.money = new System.Windows.Forms.Label();
+            this.givenMoney = new System.Windows.Forms.TextBox();
+            this.calculateChange = new System.Windows.Forms.Button();
+            this.zeroLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // title
@@ -62,7 +66,6 @@
             this.burger.Size = new System.Drawing.Size(174, 33);
             this.burger.TabIndex = 1;
             this.burger.Text = "Burger..... 2.49";
-           
             // 
             // label3
             // 
@@ -83,7 +86,6 @@
             this.label4.Size = new System.Drawing.Size(176, 33);
             this.label4.TabIndex = 3;
             this.label4.Text = "Wrap......... 2.99";
-           
             // 
             // label5
             // 
@@ -141,7 +143,7 @@
             this.newOrder.TabIndex = 11;
             this.newOrder.Text = "Place New Order";
             this.newOrder.UseVisualStyleBackColor = true;
-            this.newOrder.Click += new System.EventHandler(this.button3_Click);
+            this.newOrder.Click += new System.EventHandler(this.newOrder_Click);
             // 
             // outputLabel
             // 
@@ -152,15 +154,53 @@
             this.outputLabel.Size = new System.Drawing.Size(0, 22);
             this.outputLabel.TabIndex = 12;
             // 
-            // button1
+            // calculateTotals
             // 
-            this.button1.Location = new System.Drawing.Point(12, 413);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 42);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "Calculate Totals ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.calculateTotals.Location = new System.Drawing.Point(12, 365);
+            this.calculateTotals.Name = "calculateTotals";
+            this.calculateTotals.Size = new System.Drawing.Size(98, 42);
+            this.calculateTotals.TabIndex = 13;
+            this.calculateTotals.Text = "Calculate Totals ";
+            this.calculateTotals.UseVisualStyleBackColor = true;
+            this.calculateTotals.Click += new System.EventHandler(this.calculateTotals_Click);
+            // 
+            // money
+            // 
+            this.money.AutoSize = true;
+            this.money.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.money.Location = new System.Drawing.Point(120, 366);
+            this.money.Name = "money";
+            this.money.Size = new System.Drawing.Size(135, 38);
+            this.money.TabIndex = 14;
+            this.money.Text = "Tendered";
+            // 
+            // givenMoney
+            // 
+            this.givenMoney.Location = new System.Drawing.Point(261, 378);
+            this.givenMoney.Name = "givenMoney";
+            this.givenMoney.Size = new System.Drawing.Size(38, 20);
+            this.givenMoney.TabIndex = 15;
+            // 
+            // calculateChange
+            // 
+            this.calculateChange.Location = new System.Drawing.Point(12, 413);
+            this.calculateChange.Name = "calculateChange";
+            this.calculateChange.Size = new System.Drawing.Size(97, 42);
+            this.calculateChange.TabIndex = 16;
+            this.calculateChange.Text = "Calculate Change ";
+            this.calculateChange.UseVisualStyleBackColor = true;
+            this.calculateChange.Click += new System.EventHandler(this.calculateChange_Click);
+            // 
+            // zeroLabel
+            // 
+            this.zeroLabel.AutoSize = true;
+            this.zeroLabel.BackColor = System.Drawing.Color.Transparent;
+            this.zeroLabel.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zeroLabel.Location = new System.Drawing.Point(27, 74);
+            this.zeroLabel.Name = "zeroLabel";
+            this.zeroLabel.Size = new System.Drawing.Size(461, 30);
+            this.zeroLabel.TabIndex = 17;
+            this.zeroLabel.Text = "Please enter a zero if you dont ant an item";
             // 
             // Form1
             // 
@@ -168,7 +208,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSeaGreen;
             this.ClientSize = new System.Drawing.Size(749, 484);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zeroLabel);
+            this.Controls.Add(this.calculateChange);
+            this.Controls.Add(this.givenMoney);
+            this.Controls.Add(this.money);
+            this.Controls.Add(this.calculateTotals);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.newOrder);
             this.Controls.Add(this.print);
@@ -183,7 +227,6 @@
             this.Controls.Add(this.title);
             this.Name = "Form1";
             this.Text = "Menu";
-  
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -203,7 +246,11 @@
         private System.Windows.Forms.Button print;
         private System.Windows.Forms.Button newOrder;
         private System.Windows.Forms.Label outputLabel;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button calculateTotals;
+        private System.Windows.Forms.Label money;
+        private System.Windows.Forms.TextBox givenMoney;
+        private System.Windows.Forms.Button calculateChange;
+        private System.Windows.Forms.Label zeroLabel;
     }
 }
 
--- Cash Register/Form1.cs
+++ Cash Register/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//ICS3U Cash Registor Summative
+//Mr.T
+//October 16, 2016 
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +16,7 @@ namespace Cash_Register
 {
     public partial class Form1 : Form
     {
+        // Declare constants and variables 
         const double BURGER = 2.49;
         const double FRIES = 1.89;
         const double WRAP = 2.99;
@@ -24,45 +29,15 @@ namespace Cash_Register
         double drinkQ;
         double subtotal;
         double taxA;
+        double tendered;
+        double change;
 
         public Form1()
         {
-            InitializeComponent();
+                       InitializeComponent();
         }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                burgerQ = 0;
-                friesQ = 0;
-                wrapQ = 0;
-                drinkQ = 0;
-                burgerQ = Convert.ToInt32(inputB.Text);
-                friesQ = Convert.ToInt32(inputF.Text);
-                wrapQ = Convert.ToInt32(inputW.Text);
-                drinkQ = Convert.ToInt32(inputD.Text);
-                subtotal = burgerQ * BURGER + friesQ * FRIES + wrapQ * WRAP + drinkQ * DRINK;
-                taxA = subtotal * TAX;
-                total = subtotal + taxA; 
-
-
-                outputLabel.Text = "Subtotal                          " + subtotal.ToString("C");
-                outputLabel.Text += "\nTax                                  " + taxA.ToString("C");
-                outputLabel.Text += "\nTotal                               " + total.ToString("C");
-            }
-
-            catch // display error message if input is not a number 
-            {
-                outputLabel.Text = "You must enter an integer you silly";
-            }
-        }
-
+        
+        //creating brushes pens and fonts for the graphics
         private void print_Click(object sender, EventArgs e)
         {
             Graphics formGraphics = this.CreateGraphics();
@@ -71,8 +46,70 @@ namespace Cash_Register
             Pen blackPen = new Pen(Color.White, 10);
             Pen whitePen = new Pen(Color.White, 10);
 
-            formGraphics.FillRectangle(whiteBrush, 375, 100, 300, 325);
+            formGraphics.FillRectangle(whiteBrush, 375, 100, 300, 335);
+            Font drawFont = new Font("Courier New", 10, FontStyle.Bold);
 
+            //Use Drawstring to print reicpt
+            formGraphics.DrawString("Sam's Food Fun", drawFont, blackBrush, 470, 110);
+            formGraphics.DrawString("Order Number 107", drawFont, blackBrush, 375, 130);
+            formGraphics.DrawString("October 13, 2016", drawFont, blackBrush, 375, 150);
+            formGraphics.DrawString("Burger x " +burgerQ+"................ $"+burgerQ * BURGER, drawFont, blackBrush, 375, 190);
+            formGraphics.DrawString("Fries x " + friesQ + "................. $" + friesQ * FRIES, drawFont, blackBrush, 375, 210);
+            formGraphics.DrawString("Wraps x " + wrapQ + "................. $" + wrapQ * WRAP, drawFont, blackBrush, 375, 230);
+            formGraphics.DrawString("Drink x " + drinkQ + "................. $" + drinkQ * DRINK, drawFont, blackBrush, 375, 250);
+            formGraphics.DrawString("Subtotal ..................$" +subtotal, drawFont, blackBrush, 375, 290);
+            formGraphics.DrawString("Tax ...................... " + taxA.ToString("C"), drawFont, blackBrush, 375, 310);
+            formGraphics.DrawString("Total .................... " + total.ToString("C"), drawFont, blackBrush, 375, 330);
+            formGraphics.DrawString("Tendered ................. " + tendered.ToString("C"), drawFont, blackBrush, 375, 370);
+            formGraphics.DrawString("Change ................... " + change.ToString("C"), drawFont, blackBrush, 375, 390);
+            formGraphics.DrawString("Have A Good Day", drawFont, blackBrush, 470, 410);
+
+        }
+
+        //resets labels,text boxs, and graohics
+        private void newOrder_Click(object sender, EventArgs e)
+        {
+            inputB.Text = Convert.ToString(0);
+            inputF.Text = Convert.ToString(0);
+            inputW.Text = Convert.ToString(0);
+            inputD.Text = Convert.ToString(0);
+            givenMoney.Text = Convert.ToString(0);
+            Graphics formGraphics = this.CreateGraphics();
+            formGraphics.Clear(Color.LightSeaGreen);
+            outputLabel.Text = "    ";
+        }
+
+        //Calculates the amount of change that the costumer needs 
+        private void calculateChange_Click(object sender, EventArgs e)
+        {
+            tendered = Convert.ToInt32(givenMoney.Text);
+            change = tendered - total;
+            outputLabel.Text += "\nChange                          " + change.ToString("C");
+        }
+
+        //Calculates totals by saving textboxs to variables then using math to determine the totals
+        private void calculateTotals_Click(object sender, EventArgs e)
+        {
+            try //Determines if there is an error 
+            {
+                burgerQ = Convert.ToInt32(inputB.Text);
+                friesQ = Convert.ToInt32(inputF.Text);
+                wrapQ = Convert.ToInt32(inputW.Text);
+                drinkQ = Convert.ToInt32(inputD.Text);
+
+                subtotal = burgerQ * BURGER + friesQ * FRIES + wrapQ * WRAP + drinkQ * DRINK;
+                taxA = TAX * subtotal;
+                total = taxA + subtotal;
+
+                outputLabel.Text = "Subtotal                          " + subtotal.ToString("C");
+                outputLabel.Text += "\nTax                                  " + taxA.ToString("C");
+                outputLabel.Text += "\nTotal                               " + total.ToString("C");
+            }
+
+            catch // display error message if input is not a number 
+            {
+                outputLabel.Text = "You must enter an integer";
+            }
         }
     }
 }
